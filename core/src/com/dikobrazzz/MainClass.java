21c3eca5c82930845808e950cf52a3feabf3f7bd
dikobrazzz <41240717+dikobrazzz@users.noreply.github.com>
2019-07-10 10:45:19 +0500

    Update MainClass.java

--- core/src/com/dikobrazzz/MainClass.java
+++ core/src/com/dikobrazzz/MainClass.java
@@ -26,21 +26,23 @@ public class MainClass extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		dSizeY = Gdx.graphics.getHeight();
 		dSizeX = Gdx.graphics.getWidth();
-		collision = false;
 		sizeCircle1 = 300.0f;
 		sizeCircle2 = 200.0f;
-		circle1 = new Circle(MathUtils.random(0, dSizeX - sizeCircle1), MathUtils.random(0, dSizeY - sizeCircle1), sizeCircle1, 300.0f, 300.0f);
-		circle2 = new Circle(MathUtils.random(0, dSizeX - sizeCircle2), MathUtils.random(0, dSizeY - sizeCircle2), sizeCircle2, 400.0f, 500.0f);
+		collision = false;
+		circle1 = new Circle(MathUtils.random(0, dSizeX - sizeCircle1), MathUtils.random(0, dSizeY - sizeCircle1), sizeCircle1, 600.0f, 600.0f);
+		circle2 = new Circle(MathUtils.random(0, dSizeX - sizeCircle2), MathUtils.random(0, dSizeY - sizeCircle2), sizeCircle2, 700.0f, 800.0f);
 	}
 
 	public void update (float dt){
 		circle1.setCircleShape(circle1.getPosition().x + sizeCircle1/2, circle1.getPosition().y + sizeCircle1/2, sizeCircle1/2);
 		circle2.setCircleShape(circle2.getPosition().x+ sizeCircle2/2, circle2.getPosition().y+ sizeCircle2/2,sizeCircle2/2);
-		if (Intersector.overlaps(circle1.getCircleShape(),circle2.getCircleShape())){
-			collision = true;
+        if (Intersector.overlaps(circle1.getCircleShape(),circle2.getCircleShape())){
+            collision = true;
+        } else {
+        	collision = false;
 		}
-		circle1.update(dt);
-		circle2.update(dt);
+		circle1.update(dt, circle1.position, circle2.position);
+		circle2.update(dt, circle2.position, circle1.position);
 	}
 
 	@Override
@@ -64,7 +66,7 @@ public class MainClass extends ApplicationAdapter {
 		private float size;
 		private float speedX;
 		private float speedY;
-                private com.badlogic.gdx.math.Circle circleShape;
+		private com.badlogic.gdx.math.Circle circleShape;
 
 		public com.badlogic.gdx.math.Circle getCircleShape() {
 			return circleShape;
@@ -82,6 +84,7 @@ public class MainClass extends ApplicationAdapter {
 			velosity = new Vector2(speedX, speedY);
 			size = sizze;
 			turnX = false;
+			turnY = false;
 			this.circleShape = new com.badlogic.gdx.math.Circle(x + sizze/2, y + sizze/2, sizze/2);
 		}
 
@@ -89,42 +92,33 @@ public class MainClass extends ApplicationAdapter {
 			batch.draw(texture, position.x, position.y, size, size);
 		}
 
-		public void update (float dt){
-			collision(dt);
+		public void update (float dt, Vector2 myPos, Vector2 otherPos){
+			collision(dt, myPos, otherPos);
 		}
 
-		void collision (float dt){
+		void collision (float dt, Vector2 myPos, Vector2 otherPos){
 			if (!turnX){
 				position.x += velosity.x * dt;
 			} else {
 				position.x -= velosity.x * dt;
 			}
-			if (position.x > dSizeX - size){
+			if (position.x > dSizeX - size || (collision && myPos.x < otherPos.x)){
 				turnX = true;
 			}
-			if (position.x < 0){
+			if (position.x < 0 || (collision && myPos.x > otherPos.x)){
 				turnX = false;
 			}
 			if (!turnY){
-				if (!collision){
-					position.y += velosity.y * dt;
-				} else position.y -= velosity.y * dt;
+				position.y += velosity.y * dt;
 			} else {
-				if(!collision){
-					position.y -= velosity.y * dt;
-				} else {
-					position.y += velosity.y * dt;
-				}
+				position.y -= velosity.y * dt;
 			}
-			if (position.y < 0){
+			if (position.y < 0 || (collision && myPos.y > otherPos.y)){
 				turnY = false;
-				collision = false;
 			}
-			if (position.y > dSizeY - size){
+			if (position.y > dSizeY - size || (collision && myPos.y < otherPos.y)){
 				turnY = true;
-				collision = false;
 			}
-
 		}
 
 		Vector2 getPosition(){
