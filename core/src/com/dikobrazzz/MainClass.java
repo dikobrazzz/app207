982b3089486287d2b2fe7f7a6d64a157458ee998
dikobrazzz <41240717+dikobrazzz@users.noreply.github.com>
2019-08-05 08:40:02 +0500

    Update MainClass.java

--- core/src/com/dikobrazzz/MainClass.java
+++ core/src/com/dikobrazzz/MainClass.java
@@ -5,44 +5,59 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+
+import sun.rmi.runtime.Log;
 
 public class MainClass extends ApplicationAdapter {
 	float dSizeY;
 	float dSizeX;
 	float sizeCircle1;
 	float sizeCircle2;
+	float weightCircle1;
+	float weightCircle2;
+	float speedCircle1;
+	float speedCircle2;
 	private SpriteBatch batch;
 	private Circle circle1;
 	private Circle circle2;
 	private boolean collision;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		dSizeY = Gdx.graphics.getHeight();
 		dSizeX = Gdx.graphics.getWidth();
-		sizeCircle1 = 300.0f;
+		sizeCircle1 = 400.0f;
 		sizeCircle2 = 200.0f;
+		speedCircle1 = 500;
+		speedCircle2 = -300;
+		weightCircle1 = 6;
+		weightCircle2 = 2;
 		collision = false;
-		circle1 = new Circle(MathUtils.random(0, dSizeX - sizeCircle1), MathUtils.random(0, dSizeY - sizeCircle1), sizeCircle1, 600.0f, 600.0f);
-		circle2 = new Circle(MathUtils.random(0, dSizeX - sizeCircle2), MathUtils.random(0, dSizeY - sizeCircle2), sizeCircle2, 700.0f, 800.0f);
+		circle1 = new Circle(MathUtils.random(0, dSizeX - sizeCircle1), MathUtils.random(0, dSizeY - sizeCircle1), sizeCircle1, speedCircle1);
+		circle2 = new Circle(MathUtils.random(0, dSizeX - sizeCircle2), MathUtils.random(0, dSizeY - sizeCircle2), sizeCircle2, speedCircle2);
 	}
 
 	public void update (float dt){
 		circle1.setCircleShape(circle1.getPosition().x + sizeCircle1/2, circle1.getPosition().y + sizeCircle1/2, sizeCircle1/2);
 		circle2.setCircleShape(circle2.getPosition().x+ sizeCircle2/2, circle2.getPosition().y+ sizeCircle2/2,sizeCircle2/2);
-        if (Intersector.overlaps(circle1.getCircleShape(),circle2.getCircleShape())){
-            collision = true;
-        } else {
-        	collision = false;
+
+		if (Intersector.overlaps(circle1.getCircleShape(),circle2.getCircleShape())){
+			collision = true;
+			circle1.speed = getSpeed(speedCircle1, speedCircle2, weightCircle1, weightCircle2);
+			circle2.speed = getSpeed(speedCircle2, speedCircle1, weightCircle2, weightCircle1);
+            circle1.angle = getAngle(circle1.position, circle2.position, circle1.velocity, circle2.velocity);
+            circle2.angle = getAngle(circle2.position, circle1.position, circle2.velocity, circle1.velocity);
+		} else {
+			collision = false;
 		}
-		circle1.update(dt, circle1.position, circle2.position);
-		circle2.update(dt, circle2.position, circle1.position);
+		circle1.update(dt);
+		circle2.update(dt);
+
+
 	}
 
 	@Override
@@ -56,16 +71,31 @@ public class MainClass extends ApplicationAdapter {
 		circle2.render(batch);
 		batch.end();
 	}
+	private float getSpeed(float v1, float v2, float m1, float m2) {
+		return ((m1-m2)*v1 + 2*m2*v2)/(m1+m2);
+	}
+	private float getAngle(Vector2 pos1, Vector2 pos2, Vector2 vel1, Vector2 vel2){
+		float term1X = pos1.x;
+		float term1Y = pos1.y;
+		float term2X = pos2.x;
+		float term2Y = pos2.y;
+		double angleDest = Math.atan2(vel1.y, vel1.x)/Math.PI *180;
+		float term = pos1.sub(vel1).nor().dot(pos2.sub(vel2).nor());
+		pos1.x = term1X;
+		pos1.y = term1Y;
+		pos2.x = term2X;
+		pos2.y = term2Y;
+		float angle = (float) angleDest*term;
+		return angle;
+	}
 
 	class Circle {
 		private Texture texture;
 		private Vector2 position;
-		private Vector2 velosity;
-		private boolean turnX;
-		private boolean turnY;
+		private Vector2 velocity;
 		private float size;
-		private float speedX;
-		private float speedY;
+		float speed;
+		float angle;
 		private com.badlogic.gdx.math.Circle circleShape;
 
 		public com.badlogic.gdx.math.Circle getCircleShape() {
@@ -76,15 +106,13 @@ public class MainClass extends ApplicationAdapter {
 			circleShape.set(x, y, radius);
 		}
 
-		public Circle (float x, float y, float sizze, float speeedX, float speeedY) {
+		public Circle (float x, float y, float sizze, float speeed) {
 			texture = new Texture("circle.png");
 			position = new Vector2(x, y);
-			speedX = speeedX;
-			speedY = speeedY;
-			velosity = new Vector2(speedX, speedY);
+			speed = speeed;
+			velocity = new Vector2(speed, speed);
 			size = sizze;
-			turnX = false;
-			turnY = false;
+			angle = (float) (Math.random()*90);
 			this.circleShape = new com.badlogic.gdx.math.Circle(x + sizze/2, y + sizze/2, sizze/2);
 		}
 
@@ -92,34 +120,25 @@ public class MainClass extends ApplicationAdapter {
 			batch.draw(texture, position.x, position.y, size, size);
 		}
 
-		public void update (float dt, Vector2 myPos, Vector2 otherPos){
-			collision(dt, myPos, otherPos);
+		public void update (float dt){
+			collision(dt);
 		}
 
-		void collision (float dt, Vector2 myPos, Vector2 otherPos){
-			if (!turnX){
-				position.x += velosity.x * dt;
-			} else {
-				position.x -= velosity.x * dt;
+		void collision (float dt){
+			if (collision){
+				velocity.x = (float)Math.cos(angle)*speed;
+				velocity.y = (float)Math.sin(angle)*speed;
 			}
-			if (position.x > dSizeX - size || (collision && myPos.x < otherPos.x)){
-				turnX = true;
+			if (velocity.x > 0 && position.x > dSizeX - size || velocity.x < 0 && position.x < 0){
+				velocity.x *= -1;
 			}
-			if (position.x < 0 || (collision && myPos.x > otherPos.x)){
-				turnX = false;
+			if (velocity.y > 0 && position.y > dSizeY - size || velocity.y < 0 && position.y < 0){
+				velocity.y *= -1;
 			}
-			if (!turnY){
-				position.y += velosity.y * dt;
-			} else {
-				position.y -= velosity.y * dt;
-			}
-			if (position.y < 0 || (collision && myPos.y > otherPos.y)){
-				turnY = false;
-			}
-			if (position.y > dSizeY - size || (collision && myPos.y < otherPos.y)){
-				turnY = true;
-			}
+			position.x += velocity.x * dt;
+			position.y += velocity.y * dt;
 		}
+
 
 		Vector2 getPosition(){
 			return position;
